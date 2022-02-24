b94773c5afb8d94102856f42e71b0f9813317fa8
brahianvera <brahian.verac@gmail.com>
2022-02-23 20:49:15 -0500

    Practia arrayList
    change odd number by even number

---
+++ arrayList/src/main/java/com/mycompany/arraylist/Main.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.arraylist;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Brahian Vera
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        ArrayList<Integer> nums = new ArrayList();
+        Integer evenNumBackup =  0;
+        nums.add(1);
+        nums.add(3);
+        nums.add(2);
+        nums.add(4);
+        nums.add(7);
+        nums.add(9);
+        nums.add(11);
+        
+        for(int posEven = 0; posEven < nums.size(); posEven++){
+            if(!(nums.get(posEven) %2 == 0)){
+                continue;
+            }
+            for(int posOdd = posEven; posOdd < nums.size(); posOdd++){
+                evenNumBackup = nums.get(posEven);
+                if(!(nums.get(posOdd) %2 == 0)){
+                    nums.set(posEven,nums.get(posOdd));
+                    nums.set(posOdd,evenNumBackup);
+                    break;
+                }
+            }
+            
+        }
+        System.out.println(nums);   
+    }
+    
+}
